2b068d17defc6f9fd08bc25542fbb877883f9e8c
Samuel Jonathan <samueljonathanleumas@gmail.com>
2023-03-30 12:29:55 -0300

    Adicionando AlertDialogCamera na Home

--- app/src/main/java/com/example/login/Home.java
+++ app/src/main/java/com/example/login/Home.java
@@ -1,8 +1,12 @@
 package com.example.login;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void cameraDialog(View view){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Permissão Camera!");
+        dialog.setMessage("Para usar esse funcionalidade precisamos da permissão da camera.");
+        dialog.setIcon(android.R.drawable.ic_menu_camera);
+        dialog.setCancelable(false);
+
+        dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "Permissão concedida", Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "Permissão negada", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        dialog.create();
+        dialog.show();
     }
 }
